3980ce669761a8a966b3ab1447d7e9a63126ce72
Aluminite <10791181+Aluminite@users.noreply.github.com>
2023-09-14 01:22:53 -0400

    preliminary lab 2

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("String to test?");
+        String text = input.nextLine();
+        System.out.println(StackStuff.reverseString(text));
+        System.out.println("String is " + (StackStuff.isPalindrome(text) ? "" : "not ") + "a palindrome");
+        System.out.println("String is " + (StackStuff.isBalanced(text) ? "" : "not ") + "balanced");
     }
 }
---
+++ Lab2/src/StackStuff.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class StackStuff {
+    public static String reverseString(String input) {
+        Stack<Character> chars = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            chars.push(input.charAt(i));
+        }
+
+        String output = "";
+        while (!chars.isEmpty()) {
+            output = output + chars.pop();
+        }
+
+        return output;
+    }
+
+    public static boolean isPalindrome(String input) {
+        String lowercase = input.toLowerCase();
+        return reverseString(lowercase).equals(lowercase);
+    }
+
+    public static boolean isBalanced(String input) {
+        Stack<Character> chars = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '{' || currentChar == '[' || currentChar == '(') {
+                chars.push(currentChar);
+            } else if (currentChar == '}' || currentChar == ']' || currentChar == ')') {
+                if (chars.isEmpty()) return false;
+                char popChar = chars.pop();
+                if (popChar == '{' && currentChar != '}' ||
+                    popChar == '[' && currentChar != ']' ||
+                    popChar == '(' && currentChar != ')') {
+                    return false;
+                }
+            }
+        }
+        // if there are still characters left in the stack afterward, it's not balanced
+        return chars.isEmpty();
+    }
+}
