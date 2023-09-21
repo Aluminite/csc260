ec43766d47c1e9c4bf57e30325e308a47b82e771
Aluminite <10791181+Aluminite@users.noreply.github.com>
2023-09-21 15:48:25 -0400

    move around lab2 main

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("String to test?");
         String text = input.nextLine();
-        System.out.println("Reversed string: " + StackStuff.reverseString(text));
-        System.out.println("String is " + (StackStuff.isPalindrome(text) ? "" : "not ") + "a palindrome");
+
         System.out.println("String is " + (StackStuff.isBalanced(text) ? "" : "not ") + "balanced");
         try {
             System.out.println("Postfix evaluation of string is " + StackStuff.evaluatePostfix(text));
@@ -14,5 +14,7 @@ public class Main {
             System.out.println("Unable to evaluate postfix");
         }
         System.out.println("Infix to postfix: " + StackStuff.infixToPostfix(text));
+        System.out.println("Reversed string: " + StackStuff.reverseString(text));
+        System.out.println("String is " + (StackStuff.isPalindrome(text) ? "" : "not ") + "a palindrome");
     }
 }
